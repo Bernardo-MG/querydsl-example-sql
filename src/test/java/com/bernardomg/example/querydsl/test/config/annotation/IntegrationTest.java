b33a1a76d7ff30e1deb611ae0c1552d08963f2ba
Bernardo-MG <programming@bernardo-mg.com>
2022-12-27 23:27:46 +0100

    Cleaned up imports

--- src/test/java/com/bernardomg/example/querydsl/test/config/annotation/IntegrationTest.java
+++ src/test/java/com/bernardomg/example/querydsl/test/config/annotation/IntegrationTest.java
@@ -31,7 +31,6 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
 
